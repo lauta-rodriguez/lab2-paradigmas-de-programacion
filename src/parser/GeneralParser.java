090d5ba2dbd580379a6b93e4450e30ba707bbef0
Lautaro Rodriguez <lautaro.rodriguez@gmail.com>
2023-05-11 18:03:47 -0300

    feat: declare abstract class methods

--- src/parser/GeneralParser.java
+++ src/parser/GeneralParser.java
@@ -1,7 +1,9 @@
 package parser;
 
-
 /*Esta clase modela los atributos y metodos comunes a todos los distintos tipos de parser existentes en la aplicacion*/
 public abstract class GeneralParser {
-  /*  */
+
+  /* Este metodo debe ser implementado por todas las subclases de parser */
+  public abstract Object parse(String data);
+
 }
